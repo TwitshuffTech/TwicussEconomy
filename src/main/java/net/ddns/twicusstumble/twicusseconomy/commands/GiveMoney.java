acbbfa535b55780a05b2dc1349c8217636b2e893
nekochok0 <kkotaro2872@gmail.com>
2023-04-04 19:06:32 +0900

    メッセージを修正

--- src/main/java/net/ddns/twicusstumble/twicusseconomy/commands/GiveMoney.java
+++ src/main/java/net/ddns/twicusstumble/twicusseconomy/commands/GiveMoney.java
@@ -3,7 +3,6 @@ package net.ddns.twicusstumble.twicusseconomy.commands;
 import net.ddns.twicusstumble.twicusseconomy.util.Utils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import net.ddns.twicusstumble.twicusseconomy.system.Account;
 import net.ddns.twicusstumble.twicusseconomy.system.Money;
